f1f2305feb9935312e6625f6bbadaada1b142448
Jens Ungerer <49838243+JensUngerer@users.noreply.github.com>
2022-04-16 06:10:09 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      }
+    }
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    // allow console logging for nodejs
+    'no-console': 'off',
+
+    'import/prefer-default-export': 'off',
+
+    'padded-blocks': [
+      'error',
+      { classes: 'always' },
+    ],
+
+    // except empty line after atrribute scope variable declarations
+    '@typescript-eslint/lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+  },
+};
